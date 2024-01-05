91ce5cec37608c3c40fcab328c5e721867cd37d4
Ali Özçelik <ali.oezcelik@cosmino.de>
2024-01-05 18:19:00 +0300

    creating backend

---
+++ src/components/survey/UserExperiences.vue
@@ -0,0 +1,55 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click='loadExperiences'>Load Submitted Experiences</base-button>
+      </div>
+      <ul>
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch('https://vue-http-demo-fa50b-default-rtdb.europe-west1.firebasedatabase.app/surveys.json').then(function(response) {
+        if (response.ok) {
+          return response.json();
+        }
+      }).then(function(data) {
+        const results = [];
+        for (const id in data) {
+          results.id
+        }
+      });
+    },
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
